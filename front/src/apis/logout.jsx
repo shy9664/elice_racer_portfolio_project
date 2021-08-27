3f3dfd3608764953465ba3dff32508a410c13a39
hoyeon <ghdus223@naver.com>
2021-08-27 16:56:06 +0900

    retry

--- front/src/apis/logout.jsx
+++ front/src/apis/logout.jsx
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const logout = async () => {
     
-    const url = 'http://kdt-1st-project-74.koreacentral.cloudapp.azure.com/api/logout';
+    const url = 'kdt-1st-project-74.koreacentral.cloudapp.azure.com/api/logout';
     
     await axios.get(url);
 }
